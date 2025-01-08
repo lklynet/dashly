1b23dbe6f8860b2d4e3e04096a5a1ebb9bfc0636
LKLY <hello@leekelly.org>
2025-01-08 19:32:59 +0000

    Release v2.0.1: Support for multiple nginx databases

--- static/js/component/dragdrop.js
+++ static/js/component/dragdrop.js
@@ -21,19 +21,23 @@ function setupDragAndDrop() {
     dropZone.addEventListener("drop", async (event) => {
       event.preventDefault();
       dropZone.classList.remove("highlight");
-      const domainId = parseInt(event.dataTransfer.getData("text/plain"), 10);
+      const domainId = event.dataTransfer.getData("text/plain");
+      console.log("Dropped Domain ID:", domainId); // Debugging
+    
       const newGroup = dropZone.dataset.group;
-
+    
+      // Remove domain ID from all other groups
       Object.keys(groups).forEach((group) => {
         const index = groups[group].indexOf(domainId);
         if (index > -1) groups[group].splice(index, 1);
       });
-
+    
+      // Add domain ID to the new group if it's not already there
       if (!groups[newGroup].includes(domainId)) {
         groups[newGroup].push(domainId);
         await saveGroupsToJSON(groups);
       }
-
+    
       renderDashboard();
       setupDragAndDrop();
     });
